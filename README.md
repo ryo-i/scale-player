4d27d3bf1978db3df15bb8e64b0a3797ed1f0359
ryo-i <iidazz0109@gmail.com>
2021-11-14 19:39:05 +0900

    インドの音階を追加 #3

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Tone.jsを使ったスケールプレイヤー
 
 ## 進捗（コミット）
 
-- アラブの音階を追加（四分音は半音に置き換え） [#3](https://github.com/ryo-i/scale-player/issues/3)
+- インドの音階を追加 [#3](https://github.com/ryo-i/scale-player/issues/3)
 - スケールの種類検討中 [#2](https://github.com/ryo-i/scale-player/issues/2)
 
 ## 詳細（ブログ）
--- components/Inner.tsx
+++ components/Inner.tsx
@@ -441,6 +441,13 @@ function Inner() {
                 defaultChecked={val.defaultChecked || null} />{val.scaleName}</label></dd>
               )}
             </dl>
+            <dl id="indian_scale">
+              <dt>インドの音階</dt>
+              {inner.scaleTypeButtons.indianScale.map((val: scaleTypeButtons) =>
+                <dd><label key={val.scaleName}><input key={val.scaleName} type="radio" name="scale_type" value={val.scaleValue}  data-scale-name={val.scaleName} onChange={scaleTypeSelect}
+                defaultChecked={val.defaultChecked || null} />{val.scaleName}</label></dd>
+              )}
+            </dl>
           </div>
         </div>
       </CoadPlayer>
