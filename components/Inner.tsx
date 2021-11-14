18a7625db625baff16d6a786557402fd9aee6e17
ryo-i <iidazz0109@gmail.com>
2021-11-14 17:16:17 +0900

    ペンタトニック/ブルー・ノート・スケールを追加 #3

--- components/Inner.tsx
+++ components/Inner.tsx
@@ -420,6 +420,13 @@ function Inner() {
                 defaultChecked={val.defaultChecked || null} />{val.scaleName}</label></dd>
               )}
             </dl>
+            <dl id="pentatonic_blue_note_scale">
+              <dt>ペンタトニック/ブルー・ノート・スケール</dt>
+              {inner.scaleTypeButtons.pentatonicBlueNoteScale.map((val: scaleTypeButtons) =>
+                <dd><label key={val.scaleName}><input key={val.scaleName} type="radio" name="scale_type" value={val.scaleValue}  data-scale-name={val.scaleName} onChange={scaleTypeSelect}
+                defaultChecked={val.defaultChecked || null} />{val.scaleName}</label></dd>
+              )}
+            </dl>
           </div>
         </div>
       </CoadPlayer>
