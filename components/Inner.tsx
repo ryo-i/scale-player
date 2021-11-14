6d4b74766ab26727a9e9aab099a641ad896b469c
ryo-i <iidazz0109@gmail.com>
2021-11-15 06:47:04 +0900

    日本の音階（古典邦楽）を追加 #3

--- components/Inner.tsx
+++ components/Inner.tsx
@@ -455,6 +455,13 @@ function Inner() {
                 defaultChecked={val.defaultChecked || null} />{val.scaleName}</label></dd>
               )}
             </dl>
+            <dl id="classical_japanese_scale">
+              <dt>日本の音階（古典邦楽）</dt>
+              {inner.scaleTypeButtons.classicalJapaneseScale.map((val: scaleTypeButtons) =>
+                <dd><label key={val.scaleName}><input key={val.scaleName} type="radio" name="scale_type" value={val.scaleValue}  data-scale-name={val.scaleName} onChange={scaleTypeSelect}
+                defaultChecked={val.defaultChecked || null} />{val.scaleName}</label></dd>
+              )}
+            </dl>
           </div>
         </div>
       </CoadPlayer>
